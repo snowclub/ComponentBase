2597375aa25ccfd235bd6440da74e10a5af49ab9
ChampChicharito <c.snowclub@gmail.com>
2016-05-09 16:03:40 +0700

    add ProductRepository, productService, producrDao, productServiceImpl, productDaoIMpl, ProductController

---
+++ src/main/java/ComponentBase/product/productDaoImpl.java
@@ -0,0 +1,7 @@
+package ComponentBase.product;
+
+/**
+ * Created by TOPCOM on 9/5/2559.
+ */
+public class productDaoImpl {
+}
---
+++ src/main/java/ComponentBase/repository/productRepository.java
@@ -0,0 +1,7 @@
+package ComponentBase.repository;
+
+/**
+ * Created by TOPCOM on 9/5/2559.
+ */
+public interface productRepository {
+}
